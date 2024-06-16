65f29a9f201ef016d539f0cf68552e3aaae3d533
nesGS <nesgonsua@gmail.com>
2024-06-16 19:12:06 +0100

    estructura

---
+++ src/main/java/com/nesGS/vernaculo/VernaculoApplication.java
@@ -0,0 +1,13 @@
+package com.nesGS.vernaculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VernaculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VernaculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nesGS/vernaculo/VernaculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesGS.vernaculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VernaculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
